18494283f162bf21053ba80db915565ad46d97a6
DaveYYC <DaveYYC@users.noreply.github.com>
2020-07-11 00:43:16 -0600

    completed gallery section and final tweaks

--- READEME.md
+++ READEME.md
@@ -11,19 +11,14 @@ Submission Date: June 9, 2020
 
 Acknowledgements:
 
-    Company logo courtesy of http://.vecteezy.com
+    -Company logo courtesy of http://.vecteezy.com
 
-    Hero Section image courtesy of Lorem Picsum: https://picsum.photos/
+    -Hero section and gallery images courtesy of Lorem Picsum: https://picsum.photos/
 
-    Destination images courtesy of http://pexels.com
-
-    How to Populate HTML table from Array using Javascript Author: 1BestCsharp blog
-
-
-    Javascript for invalid form entry red border color:  https://daverupert.com/2017/11/happier-html5-forms/
+    -Javascript for invalid form entry red border color:  https://daverupert.com/2017/11/happier-html5-forms/
 
 *** I could make a red border for invalid form entries in CSS, however the border was red upon page loading. 
-The javascript allows the border to remain black until the submit button is pressed. 
+The javascript allows the border to remain black until the submit button is pressed.*** 
 
 
 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
---
+++ main.js
@@ -0,0 +1,49 @@
+
+/* ---- Image Gallery ---- */
+
+const imgList = [
+
+    [1022, '  Alaska',  'picture of northern lights',       'https://www.travelalaska.com'],
+    [1029, '  New York','picture of new york central park', 'https://www.ny.gov/'],
+    [1040, '  Germany', 'picture of german castle',         'https://www.germany.travel/en/index.html'],
+    [164,  '  Italy',   'picture of italy landscape',                 'http://www.italia.it/en/home.html'],
+    [234,  '  Paris',   'picture of eiffel tower in paris', 'https://en.parisinfo.com/']
+];
+
+let imgTemplate = ''; 
+
+const gallery = document.querySelector('.gallery'); 
+
+imgList.forEach(function(item){
+
+    imgTemplate += `<a href="${item[3]}"><img src="https://picsum.photos/id/${item[0]}/225/225">
+    Click for information about${item[1]}<alt="${item[2]}">`;
+    
+    // console.log(imgTemplate);
+});
+
+gallery.innerHTML = imgTemplate;
+
+/* ---- Hidden Menu Button ---- */
+
+const button = document.querySelector('.btn');
+const menu = document.querySelector('nav ul');
+
+    function eventHandler() {
+        menu.classList.toggle('visible');
+}
+
+button.addEventListener('click', eventHandler);
+
+
+
+
+
+
+
+
+
+
+
+
+
