643ebe47d7ec0ac8389c00cbba147f43f6dfb8f9
DaveYYC <DaveYYC@users.noreply.github.com>
2020-07-13 12:38:18 -0600

    testing pug

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Endpoint handler for the home page
-app.get('/', function(request, response){
+app.get('/index.html', function(request, response){
   response.render('index.pug',{});
 })
 
